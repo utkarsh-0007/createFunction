8f51f8b7d808a92849ba3e27ccc7d4023e54c80a
Utkarsh <lovekickstartsyadav@gmail.com>
2024-04-20 01:57:56 +0530

    cb function

--- cbFunction.js
+++ cbFunction.js
@@ -1,3 +1,2 @@
 export default function cb(value, index, items){
-    console.log(`The elememt ${value} is at ${index} index of items`);
-}
+    console.log(`The elememt ${value} is at ${index} index of items`);}
